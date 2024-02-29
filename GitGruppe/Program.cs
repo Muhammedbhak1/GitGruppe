a79c1164111bdcf16ffcfd97155f40e8c2ed826a
muhammed <mariduru@bhakwien10.at>
2024-02-29 10:29:40 +0100

    Änderung4

--- GitGruppe/Program.cs
+++ GitGruppe/Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine("Änderung1");
             Console.WriteLine("Änderung2");
             Console.WriteLine("Änderung3");
+            Console.WriteLine("Änderung4+Fehler");
         }
     }
 }
